d188bed3ffa8e5205fd7dcff30dc388e2cb6e194
Ahmadjanowh <h.ahmadjanow@bk.ru>
2024-01-01 15:30:22 +0200

    for while

---
+++ base/lesson4.py
@@ -0,0 +1,22 @@
+# Циклы - фрагмент кода котрое выполноестья многократно 
+
+# for days in range(1,31):
+#     print(days)
+
+# name = input("Ей имю: ")
+# for i in range(1,101):
+#     print(i,"I love you",name)
+
+n = 1
+while n <= 10:
+    if n != 5:
+        print(n)
+    n += 1
+
+i = 1
+while True:
+    if i > 1000:
+        break
+    print(i)
+    i += 1 
+ 
